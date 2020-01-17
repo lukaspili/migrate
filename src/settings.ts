90f909681c262903c4548625b2da0484b05830ea
Lukasz <lukasz@intheloup.io>
2020-01-17 18:34:57 -0500

    Fully working POC.

--- src/settings.ts
+++ src/settings.ts
@@ -291,17 +291,3 @@ export async function parseSettings(
   };
   /* eslint-enable */
 }
-
-export function getCurrentMigrationPath(
-  parsedSettings: ParsedSettings
-): string {
-  return `${parsedSettings.migrationsFolder}/current.sql`;
-}
-
-export function getCurrentMigrationFolderPath(
-  parsedSettings: ParsedSettings
-): string {
-  return `${parsedSettings.migrationsFolder}/current`;
-}
-
-export const BLANK_MIGRATION_CONTENT = "-- Enter migration here\n";
